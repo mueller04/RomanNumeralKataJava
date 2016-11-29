93df4ce05c68615177bac66f28d6c375311c2cc7
Mike Mueller <mikemueller04@hotmail.com>
2016-11-29 18:47:11 -0500

    adding throws exception

--- src/test/java/AdditionTest.java
+++ src/test/java/AdditionTest.java
@@ -3,7 +3,9 @@ package test.java;
 import main.java.Calculator;
 import main.java.RomanNumeralConverter;
 import main.java.RomanNumerals;
+import org.junit.Rule;
 import org.junit.Test;
+import org.junit.rules.ExpectedException;
 
 import static org.junit.Assert.assertEquals;
 
@@ -12,6 +14,9 @@ public class AdditionTest {
     RomanNumerals romanNumerals = new RomanNumerals();
     RomanNumeralConverter romanNumeralConverter = new RomanNumeralConverter(romanNumerals);
     Calculator calculator = new Calculator(romanNumeralConverter);
+
+    @Rule
+    public ExpectedException thrown = ExpectedException.none();
 
     @Test
     public void testIPlusIEqualsII(){
@@ -29,5 +34,12 @@ public class AdditionTest {
     public void testXLVIIPlusCLEqualsCXCIII(){
         String result = calculator.add("XLVII", "CL");
         assertEquals("CXCVII", result);
+    }
+
+    @Test
+    public void testMMMPlusMMThrowsException(){
+        thrown.expect(IllegalArgumentException.class);
+        thrown.expectMessage("int used for toNumeral(int) cannot exceed 3999");
+        calculator.add("MMM", "MM");
     }
 }
