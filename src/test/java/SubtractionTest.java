25517b2063b094fb07f98f47a1ecc03cb947ce57
Mike Mueller <mikemueller04@hotmail.com>
2016-11-29 21:35:55 -0500

    another subtract test

--- src/test/java/SubtractionTest.java
+++ src/test/java/SubtractionTest.java
@@ -12,10 +12,16 @@ public class SubtractionTest {
     RomanNumerals romanNumerals = new RomanNumerals();
     RomanNumeralConverter romanNumeralConverter = new RomanNumeralConverter(romanNumerals);
     Calculator calculator = new Calculator(romanNumeralConverter);
-    
+
     @Test
     public void testIIIMinusIEqualsII(){
         String result = calculator.subtract("III", "I");
         assertEquals("II", result);
+    }
+
+    @Test
+    public void testMMCXMinusCXEqualsMM(){
+        String result = calculator.subtract("MMCX", "CX");
+        assertEquals("MM", result);
     }
 }
