e4332f9fce1721b8c2aeb3482cca6130ce6e6fb2
Mike Mueller <mikemueller04@hotmail.com>
2016-11-29 21:36:54 -0500

    spacing'

--- src/main/java/Calculator.java
+++ src/main/java/Calculator.java
@@ -17,7 +17,6 @@ public class Calculator {
     }
 
     public String subtract(String numeral1Str, String numberal2Str) {
-
         int integer1 = romanNumeralConverter.toInt(numeral1Str);
         int integer2 = romanNumeralConverter.toInt(numberal2Str);
         int additionResult = integer1 - integer2;
